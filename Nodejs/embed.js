f8c4821b810fbdfc3f19fea9db25129c40f7ba83
Nithya <nithya.gopal@syncfusion.com>
2023-12-06 13:36:33 +0530

    Restrict-to-pass-serverdetails-to-client-app-vue-with-nodejs

--- Nodejs/embed.js
+++ Nodejs/embed.js
@@ -1,67 +1,72 @@
- var fs = require("fs");
-    var http = require("http");
-    var https = require("https");
-    var url = require("url");
-    var express = require('express');
-    var cors = require('cors');
-    var app = express();
-    var crypto = require('crypto');
-    const path = require('path');
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var crypto = require('crypto');
+const path = require('path');
 
-    app.use(cors());
-    //Parse JSON bodies (as sent by API clients).
-    app.use(express.json());
+app.use(cors());
+//Parse JSON bodies (as sent by API clients).
+app.use(express.json());
 
-    //Assign a port number for an API to run.
-    const port = 8080;
+//Assign a port number for an API to run.
+const port = 8080;
 
-    let appconfig;
-    try {
+let appconfig;
+try {
     appconfig = JSON.parse(fs.readFileSync('embedConfig.json'));
-    } catch (error) {
+} catch (error) {
     console.error('Error: embedConfig.json file not found.');
     process.exit(1);
-    }
+}
 
-    var embedSecret = appconfig.EmbedSecret;
-    var userEmail = appconfig.UserEmail;
+var embedSecret = appconfig.EmbedSecret;
+var userEmail = appconfig.UserEmail;
 
-    app.post('/authorizationserver',  async function (req, response){
+app.post('/authorizationserver', async function (req, response) {
     var embedQuerString = req.body.embedQuerString;
     var dashboardServerApiUrl = req.body.dashboardServerApiUrl;
 
     embedQuerString += "&embed_user_email=" + userEmail;
     embedQuerString += "&embed_server_timestamp=" + Math.round((new Date()).getTime() / 1000);
     var embedSignature = "&embed_signature=" + GetSignatureUrl(embedQuerString);
-    var embedDetailsUrl = "/embed/authorize?" + embedQuerString+embedSignature;
+    var embedDetailsUrl = "/embed/authorize?" + embedQuerString + embedSignature;
 
     var serverProtocol = url.parse(dashboardServerApiUrl).protocol == 'https:' ? https : http;
-    serverProtocol.get(dashboardServerApiUrl+embedDetailsUrl, function(res){
+    serverProtocol.get(dashboardServerApiUrl + embedDetailsUrl, function (res) {
         var str = '';
         res.on('data', function (chunk) {
-        str += chunk;
+            str += chunk;
         });
         res.on('end', function () {
-        response.send(str);
+            response.send(str);
         });
     });
-    })
+})
 
     function GetSignatureUrl(queryString)
     {
     var keyBytes = Buffer.from(embedSecret);
     var hmac = crypto.createHmac('sha256', keyBytes);
     data = hmac.update(queryString);
-    gen_hmac= data.digest().toString('base64');
+    gen_hmac = data.digest().toString('base64');
 
     return gen_hmac;
-    }
-    app.get('/getdetails', (req, res) => {
-    const embedConfigPath = path.join(__dirname, 'embedConfig.json');
-    const jsonData = fs.readFileSync(embedConfigPath, 'utf8');
-    res.send(jsonData);
-    });
+}
+app.get('/getdetails', (req, res) => {
+    const serverEmbedConfigData = path.join(__dirname, 'embedConfig.json');
+    const jsonData = fs.readFileSync(serverEmbedConfigData, 'utf8');
+    const parsedData = JSON.parse(jsonData);
 
-    app.listen(port, () => {
+    const clientEmbedConfigData = {
+        DashboardId: parsedData.DashboardId, ServerUrl: parsedData.ServerUrl, SiteIdentifier: parsedData.SiteIdentifier, EmbedType: parsedData.EmbedType, Environment: parsedData.Environment
+    };
+    res.send(clientEmbedConfigData);
+});
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    }); 
+}); 
